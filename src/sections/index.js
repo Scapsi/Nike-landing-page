8ba423e0277966be70054dcdce7b5e5ccf13072d
scapsi <frankuladpp@gmail.com>
2023-11-27 08:50:05 +0100

    complete nike page

---
+++ src/sections/index.js
@@ -0,0 +1,18 @@
+import Hero from './Hero';
+import CustomerReview from './CustomerReview';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+import SpecialOffer from './SpecialOffer';
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Subscribe,
+  Footer,
+  Services,
+  CustomerReview,
+  SpecialOffer,
+};
